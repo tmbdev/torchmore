fdb6cd33dc60d55d537830d6fb569591678e2dd9
Tom <tmbdev+github@gmail.com>
2020-07-18 09:51:09 -0700

    code cleanup

--- torchmore/flex.py
+++ torchmore/flex.py
@@ -6,16 +6,13 @@
 
 # TODO transfer the documentation strings to flex creators/instances
 
-import numpy as np
 import torch
 from torch import autograd, nn
-from torch.autograd import Variable
-import warnings
-from functools import wraps
 
-from . import helpers, layers
+from . import layers
 
 verbose = False
+
 
 class Flex(nn.Module):
     def __init__(self, creator):
@@ -26,28 +23,30 @@ class Flex(nn.Module):
     def forward(self, *args):
         if self.layer is None:
             self.layer = self.creator(*args)
-            if verbose: print("# created", self.layer)
+            if verbose:
+                print("# created", self.layer)
         return self.layer.forward(*args)
 
     def __repr__(self):
-        return "Flex:"+repr(self.layer)
+        return "Flex:" + repr(self.layer)
 
     def __str__(self):
-        return "Flex:"+str(self.layer)
+        return "Flex:" + str(self.layer)
 
 
 def Linear(*args, **kw):
     def creator(x):
         assert x.ndimension() == 2
         return nn.Linear(x.size(1), *args, **kw)
+
     return Flex(creator)
 
 
 def Conv1d(*args, **kw):
     def creator(x):
         assert x.ndimension() == 3
-        d = x.size(1)
         return nn.Conv1d(x.size(1), *args, **kw)
+
     return Flex(creator)
 
 
@@ -55,6 +54,7 @@ def Conv2d(*args, **kw):
     def creator(x):
         assert x.ndimension() == 4
         return nn.Conv2d(x.size(1), *args, **kw)
+
     return Flex(creator)
 
 
@@ -62,14 +62,15 @@ def Conv3d(*args, **kw):
     def creator(x):
         assert x.ndimension() == 5
         return nn.Conv3d(x.size(1), *args, **kw)
+
     return Flex(creator)
 
 
 def ConvTranspose1d(*args, **kw):
     def creator(x):
         assert x.ndimension() == 3
-        d = x.size(1)
         return nn.ConvTranspose1d(x.size(1), *args, **kw)
+
     return Flex(creator)
 
 
@@ -77,6 +78,7 @@ def ConvTranspose2d(*args, **kw):
     def creator(x):
         assert x.ndimension() == 4
         return nn.ConvTranspose2d(x.size(1), *args, **kw)
+
     return Flex(creator)
 
 
@@ -84,6 +86,7 @@ def ConvTranspose3d(*args, **kw):
     def creator(x):
         assert x.ndimension() == 5
         return nn.ConvTranspose3d(x.size(1), *args, **kw)
+
     return Flex(creator)
 
 
@@ -91,37 +94,48 @@ def LSTM(*args, **kw):
     def creator(x):
         assert x.ndimension() == 3
         return layers.LSTM(x.size(2), *args, **kw)
+
     return Flex(creator)
+
 
 def BDL_LSTM(*args, **kw):
     def creator(x):
         assert x.ndimension() == 3
         return layers.BDL_LSTM(x.size(1), *args, **kw)
+
     return Flex(creator)
+
 
 Lstm1 = BDL_LSTM
 Lstm1d = BDL_LSTM
+
 
 def BDHW_LSTM(*args, **kw):
     def creator(x):
         assert x.ndimension() == 4
         return layers.BDHW_LSTM(x.size(1), *args, **kw)
+
     return Flex(creator)
+
 
 Lstm2 = BDHW_LSTM
 Lstm2d = BDHW_LSTM
+
 
 def BatchNorm(*args, **kw):
     def creator(x):
         assert x.ndimension() == 2
         assert x.size(0) > 1, f"batch norm requires batch size > 1, got {x.shape}"
         return nn.BatchNorm1d(x.size(1), *args, **kw)
+
     return Flex(creator)
+
 
 def BatchNorm1d(*args, **kw):
     def creator(x):
         assert x.ndimension() == 3
         return nn.BatchNorm1d(x.size(1), *args, **kw)
+
     return Flex(creator)
 
 
@@ -129,6 +143,7 @@ def BatchNorm2d(*args, **kw):
     def creator(x):
         assert x.ndimension() == 4
         return nn.BatchNorm2d(x.size(1), *args, **kw)
+
     return Flex(creator)
 
 
@@ -136,6 +151,7 @@ def BatchNorm3d(*args, **kw):
     def creator(x):
         assert x.ndimension() == 5
         return nn.BatchNorm3d(x.size(1), *args, **kw)
+
     return Flex(creator)
 
 
@@ -155,11 +171,14 @@ def flex_replacer(module):
     else:
         return None
 
+
 def flex_freeze(model):
     replace_modules(model, flex_replacer)
 
+
 def freeze(model):
     replace_modules(model, flex_replacer)
+
 
 def shape_inference(model, tensor, dtype=None):
     if isinstance(tensor, (tuple, list)):
@@ -168,6 +187,7 @@ def shape_inference(model, tensor, dtype=None):
     with autograd.no_grad():
         model(tensor)
     replace_modules(model, flex_replacer)
+
 
 def delete_modules(model, f):
     for key in list(model._modules.keys()):
